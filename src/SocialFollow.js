3fbe5cf6bd85a01b111291bf5ee65c7b5913bafe
Antonio Muniz <antonio.gustavo_@live.com>
2020-12-26 15:02:09 -0300

    Fixed bottom tab

--- src/SocialFollow.js
+++ src/SocialFollow.js
@@ -13,18 +13,18 @@ function SocialFollow() {
 
         <a href="https://www.youtube.com/"
             className="whatsapp social">
-            <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+            <FontAwesomeIcon icon={faWhatsapp} />
         </a>
         <a href="https://www.facebook.com/"
           className="facebook social">
-          <FontAwesomeIcon icon={faFacebook} size="2x" />
+          <FontAwesomeIcon icon={faFacebook} />
         </a>
         <a href="https://www.twitter.com/" className="twitter social">
-          <FontAwesomeIcon icon={faTwitter} size="2x" />
+          <FontAwesomeIcon icon={faTwitter} />
         </a>
         <a href="https://www.instagram.com/"
           className="instagram social">
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
+          <FontAwesomeIcon icon={faInstagram} />
         </a>
     </div>
   )
